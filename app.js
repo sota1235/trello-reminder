781f5bfeb1b127cc14cb33437a61b85237e221f8
sota1235 <sota1235@gmail.com>
2016-08-10 00:52:08 +0900

    modify: parse request body from line

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import koa       from 'koa';
 import koaRouter from 'koa-router';
 import koaBody   from 'koa-body';
+import _         from 'lodash';
 
 const app = koa();
 const router = koaRouter();
@@ -12,6 +13,8 @@ router.get('/', function *() {
 
 router.post('/callback', bodyParser, function *() {
   console.log(this.request.body);
+  const msg = _.get(this.request.body, 'result[0].content.text', null);
+  console.log(msg);
   this.status = 200;
 });
 
